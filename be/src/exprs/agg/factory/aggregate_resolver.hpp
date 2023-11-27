ac3c53a08feed10bd741b1249de7e6114ab6287a
zenoyang <cookie.yz@qq.com>
2023-11-27 20:11:07 +0800

    f
    Signed-off-by: zenoyang <cookie.yz@qq.com>

--- be/src/exprs/agg/factory/aggregate_resolver.hpp
+++ be/src/exprs/agg/factory/aggregate_resolver.hpp
@@ -73,7 +73,8 @@ public:
         const static std::vector<LogicalType> kTypes{
                 TYPE_BOOLEAN,   TYPE_TINYINT,   TYPE_SMALLINT,  TYPE_INT,        TYPE_BIGINT, TYPE_LARGEINT,
                 TYPE_FLOAT,     TYPE_DOUBLE,    TYPE_VARCHAR,   TYPE_CHAR,       TYPE_DATE,   TYPE_DATETIME,
-                TYPE_DECIMALV2, TYPE_DECIMAL32, TYPE_DECIMAL64, TYPE_DECIMAL128, TYPE_HLL,    TYPE_OBJECT};
+                TYPE_DECIMALV2, TYPE_DECIMAL32, TYPE_DECIMAL64, TYPE_DECIMAL128, TYPE_HLL,    TYPE_OBJECT,
+                TYPE_STRUCT};
         return kTypes;
     }
 
